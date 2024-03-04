6a3f95d0bd6d7fcc53b8f703703c8165e0a81404
Inna Artemenko <innochka_va@inbox.ru>
2024-03-04 23:20:39 +0500

    add barrier

--- Program.cs
+++ Program.cs
@@ -8,70 +8,138 @@ using System.IO;
 
 namespace App
 {
-    class Description_of_point
+    class Bird
     {
-        public double x;
-        public double y;
-        public double Vx;
-        public double Vy;
-
-        public Description_of_point(double x, double y, double Vx, double Vy)
+        class Description_of_point
         {
-            this.x = x;
-            this.y = y;
-            this.Vx = Vx;
-            this.Vy = Vy;
-        }
-    }
-    class Parabola_Flight
-    {
-        readonly double V0;
-        readonly double alpha;
-        readonly double m;
-        readonly double g = 9.8;
-        public List<Description_of_point> Points = new List<Description_of_point> { };
-        double partition = 1;
-        double k = 1 / 2;
+            public double x;
+            public double y;
+            public double Vx;
+            public double Vy;
 
-        public Parabola_Flight(double V0, double alpha, double m, double partition)
-        {
-            this.V0 = V0;
-            this.alpha = alpha;
-            this.m = m;
-            this.partition = partition;
-        }
-
-        public Description_of_point Position(List<Description_of_point> Points, Int32 i, double dt,  double m, double k)
-        {
-            double x = Points[i - 1].x + dt * Points[i - 1].Vx;
-            double y = Points[i - 1].y + dt * Points[i - 1].Vy;
-
-            double Vx = Points[i - 1].Vx - k * dt * Points[i - 1].Vx / m;
-            double Vy = Points[i - 1].Vy - dt * (g + k * Points[i - 1].Vy / m);
-
-            return new Description_of_point(x, y, Vx, Vy);
-        }
-
-        public void Calculate(double x0, double y0)
-        {
-            double t = 0;
-            double T = 2 * V0 * Math.Sin(alpha) / g;
-            double dt = T / partition;
-
-            Description_of_point point = new Description_of_point(x0, y0, V0 * Math.Cos(alpha), V0 * Math.Sin(alpha));
-            Points.Add(point);
-            t += dt;
-            Int32 i = 1;
-
-            while (t < T)
+            public Description_of_point(double x, double y, double Vx, double Vy)
             {
-                double k = 1 / 2;
-                Description_of_point position = Position(Points, i, dt, m, k);
-                Points.Add(position);
+                this.x = x;
+                this.y = y;
+                this.Vx = Vx;
+                this.Vy = Vy;
+            }
+        }
+        class Barrier
+        {
+            double distance, height;
+            public Barrier(double distance, double height)
+            {
+                this.distance = distance;
+                this.height = height;
+            }
+            public bool Collision(double x, double y)
+            {
+                if (x == distance && y <= height) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        class Parabola_Flight
+        {
+            readonly double V0;
+            readonly double alpha;
+            readonly double m;
+            readonly double g = 9.8;
+            public List<Description_of_point> Points = new List<Description_of_point> { };
+            double partition = 1;
+            double k = 1 / 2;
+
+            public Parabola_Flight(double V0, double alpha, double m, double partition)
+            {
+                this.V0 = V0;
+                this.alpha = alpha;
+                this.m = m;
+                this.partition = partition;
+            }
+
+            public Description_of_point Position(List<Description_of_point> Points, Int32 i, double dt, double m, double k, Barrier barrier)
+            {
+                if (Points[i - 1].x != double.PositiveInfinity && Points[i - 1].y != double.PositiveInfinity){
+                    double x = Points[i - 1].x + dt * Points[i - 1].Vx;
+                    double y = Points[i - 1].y + dt * Points[i - 1].Vy;
+
+                    double Vx = Points[i - 1].Vx - k * dt * Points[i - 1].Vx / m;
+                    double Vy = Points[i - 1].Vy - dt * (g + k * Points[i - 1].Vy / m);
+                    if (barrier.Collision(x, y))
+                    {
+                        Points[i].x = double.PositiveInfinity;
+                        Points[i].y = double.PositiveInfinity;
+                    }
+                    return new Description_of_point(x, y, Vx, Vy); 
+                }  
+                else
+                {
+                    return new Description_of_point(0, 0, 1, 1);
+                }
+            }
+
+            public void Calculate(double x0, double y0)
+            {
+                double t = 0;
+                double T = 2 * V0 * Math.Sin(alpha) / g;
+                double dt = T / partition;
+
+
+                Description_of_point point = new Description_of_point(x0, y0, V0 * Math.Cos(alpha), V0 * Math.Sin(alpha));
+                Points.Add(point);
+                double[,] positions_barriers = new double[2, 2] { { 1, 2 }, { 3, 5 } };
+                Barrier barrier = new Barrier(positions_barriers[0,0], positions_barriers[0,1]);
                 t += dt;
-                i += 1;
+                Int32 i = 1;
+                double k = 1 / 2;
+
+                while (t < T)
+                {
+                    Description_of_point position = Position(Points, i, dt, m, k, barrier);
+                    Points.Add(position);
+                    t += dt;
+                    i += 1;
+                }
+            }
+            public void Read(string path) {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string s;
+
+                    while (((s = sr.ReadLine()) != null))
+                    {
+                        string[] subs = s.Split();
+
+                        if (!double.TryParse(subs[0], out double X))
+                        {
+                            return;
+                        }
+
+                        if (!double.TryParse(subs[1], out double Y))
+                        {
+                            return;
+                        }
+
+                        Description_of_point p = new Description_of_point(X, Y, 0, 0);
+                        Points.Add(p);
+                    }
+                }
+            }
+            public async void Write(string path)
+            {
+                using (StreamWriter outputFile = new StreamWriter(path))
+                {
+                    foreach (Description_of_point p in Points)
+                    {
+                        await outputFile.WriteLineAsync($"{p.x}, {p.y}");
+                    }
+                }
             }
         }
     }
-
 }
+
+
+
